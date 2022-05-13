6fac671374f86a7f2e036cda7cd602dde41afdb7
Sai Bingi <saibingi1208@gmail.com>
2022-05-13 12:59:56 +0000

    StopWatch Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==> https://dgitalstopwatch.ccbp.tech
+
+import Stopwatch from './components/Stopwatch'
+
+import './App.css'
+
+const App = () => <Stopwatch />
+
+export default App
